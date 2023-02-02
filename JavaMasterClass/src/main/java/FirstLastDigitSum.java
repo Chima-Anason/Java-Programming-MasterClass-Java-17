4503f09c9ff74836536e6be5610c6d079ba0c2fb
Chima Anason <tblynxx@gmail.com>
2023-02-03 06:44:48 +0800

    NumberPalindromeChallenge

---
+++ JavaMasterClass/src/main/java/FirstLastDigitSum.java
@@ -0,0 +1,24 @@
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number){
+        if(number<0){
+            return -1;
+        }else if(number==0){
+            return 0;
+        }
+
+        int lastDigit = number%10;
+        int firstDigit = 0;
+
+        while(number>0){
+            firstDigit = number%10;
+            number /= 10;
+        }
+        System.out.println(firstDigit+lastDigit);
+        return firstDigit+lastDigit;
+    }
+
+
+    public static void main(String[] args) {
+        sumFirstAndLastDigit(5);
+    }
+}
