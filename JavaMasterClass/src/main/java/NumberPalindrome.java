3acc074abc1f6cca675edb3e2b773dd77a232c98
Chima Anason <tblynxx@gmail.com>
2023-02-03 06:28:50 +0800

    NumberPalindromeChallenge

---
+++ JavaMasterClass/src/main/java/NumberPalindrome.java
@@ -0,0 +1,25 @@
+public class NumberPalindrome {
+
+    public static boolean isPalindrome(int number){
+        String actualNumber = String.valueOf(number);
+        int polNumber = Math.abs(number);
+        String reverseNumber = "";
+        int lastNum = 0;
+
+        if(number<0){
+            reverseNumber += "-";
+        }
+        while(polNumber>0){
+            reverseNumber += (polNumber%10);
+            polNumber/=10;
+        }
+        if(reverseNumber.equals(actualNumber)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-707));
+    }
+}
