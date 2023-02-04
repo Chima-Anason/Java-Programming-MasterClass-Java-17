0867e1c5d1eb4ef9ac2611794ea755bc591d0cfc
Chima Anason <tblynxx@gmail.com>
2023-02-05 03:54:41 +0800

    PerfectNumber

---
+++ JavaMasterClass/src/main/java/PerfectNumber.java
@@ -0,0 +1,27 @@
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber(int number){
+        if(number<1){
+            return false;
+        }
+
+        int sum = 0;
+        for(int i=1; i<number; i++){
+            if(number%i == 0){
+                sum += i;
+                System.out.println(i);
+            }
+
+        }
+        if(sum==number){
+            System.out.println("Sum : " + sum);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(-1));
+    }
+}
