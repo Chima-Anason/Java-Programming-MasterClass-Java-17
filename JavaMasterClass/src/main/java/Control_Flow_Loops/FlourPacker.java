d0d939455a44709ba002332e1f41b2a5417d226c
Chima-Anason <tblynxx@gmail.com>
2023-02-08 23:38:02 +0800

    FlourPacker found solution

--- JavaMasterClass/src/main/java/Control_Flow_Loops/FlourPacker.java
+++ JavaMasterClass/src/main/java/Control_Flow_Loops/FlourPacker.java
@@ -4,15 +4,40 @@ public class FlourPacker {
 
     //Still trying to figure out what the problem mean
 
-    public static boolean canPack(int bigCount, int smallCount, int goal){
-        bigCount *= 5;
-        int sum = bigCount+smallCount;
-
-        if(goal == sum){
-            return true;
-        }else{
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        boolean result = false;
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return result;
+        }
+        if ((bigCount * 5) + (smallCount) >= goal) {
+            result = true;
+            while (goal > 0) {
+                if (goal >= 5) {
+                    if(bigCount >= 1) {
+                        goal -= 5;
+                        bigCount--;
+                    }
+                    else if (smallCount >= goal) {
+                        goal = 0;
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+                else if (goal < 5) {
+                    if (smallCount >= goal) {
+                        goal = 0;
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return result;
+        } else {
             return false;
         }
+
     }
 
     public static void main(String[] args) {
